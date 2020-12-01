e1dcd21a5b619444aca5f4292c2af952d3483a7e
Scott <se.brink15@gmail.com>
2020-11-30 23:43:57 -0600

    finished day01 and added some basic documentation and a little bit of code cleanup

--- day01/day01.nim
+++ day01/day01.nim
@@ -1,21 +1,20 @@
 # Day01 Advent of Code 2020
-
 import strutils, sequtils
 
+# Parse part one
 proc partOne(file: seq[int]): int =
     for i in file:
         for j in file:
             if i+j == 2020:
-                result += i*j
-                return
+                return i*j
 
+# Parse part two
 proc partTwo(file: seq[int]): int =
     for i in file:
         for j in file:
             for k in file:
                 if i+j+k == 2020:
-                    result += i*j*k
-                    return
+                    return i*j*k
     
 # Read in input file and converts everything to numbers
 let input = readFile("input.txt").strip().splitLines().map(parseInt)
